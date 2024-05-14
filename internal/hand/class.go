c97517114e1f5077641419204a15d6a6eaffc5ca
jqwez <jqwez@icloud.com>
2024-05-13 21:49:58 -0400

    added straight flush and best hand function

--- internal/hand/class.go
+++ internal/hand/class.go
@@ -1,11 +1,79 @@
 package hand
 
-import "github.com/jqwez/pokergopher/internal/deck"
+import (
+	"fmt"
+	"github.com/jqwez/pokergopher/internal/deck"
+)
 
-type Classer struct{}
+type HandClass int
+
+const (
+	ROYAL HandClass = iota
+	SFLUSH
+	QUADS
+	FHOUSE
+	FLUSH
+	STRAIGHT
+	TRIPS
+	TWOPAIR
+	PAIR
+	NOPAIR
+)
+
+type Classer struct {
+	Hand  *Hand
+	Class HandClass
+}
 
 func NewClasser() *Classer {
 	return &Classer{}
+}
+
+func NewClasserH(h *Hand) *Classer {
+	c := NewClasser()
+	return c.SetHand(h)
+}
+
+func (c *Classer) SetHand(h *Hand) *Classer {
+	c.Hand = h
+	c.findBestHand()
+	return c
+}
+
+func (c *Classer) findBestHand() *Classer {
+	isStraightFlush, straightFlush := c.IsStraightFlush(c.Hand)
+	if isStraightFlush {
+		c.Hand = straightFlush
+		c.Class = SFLUSH
+		if c.Hand.Cards[4].Rank == deck.TEN {
+			c.Class = ROYAL
+		}
+		return c
+	}
+	isFlush, flush := c.IsFlush(c.Hand)
+	if isFlush {
+		c.Hand = flush
+		c.Class = FLUSH
+		return c
+	}
+	isStraight, straight := c.IsStraight(c.Hand)
+	if isStraight {
+		c.Hand = straight
+		c.Class = STRAIGHT
+		return c
+	}
+
+	c.Hand.Cards = c.Hand.Cards[:5]
+	c.Class = NOPAIR
+	return c
+}
+
+func (c *Classer) IsStraightFlush(h *Hand) (bool, *Hand) {
+	res, straightFlush := c.IsStraight(h.FilterSuit(h.Cards[0].Suit))
+	if res {
+		return true, straightFlush
+	}
+	return false, h
 }
 
 func (c *Classer) IsFlush(_hand *Hand) (bool, *Hand) {
@@ -13,6 +81,8 @@ func (c *Classer) IsFlush(_hand *Hand) (bool, *Hand) {
 	for _, card := range _hand.Cards {
 		suitCounter[card.Suit] = suitCounter[card.Suit] + 1
 		if suitCounter[card.Suit] >= 5 {
+			_hand = _hand.FilterSuit(card.Suit).SortByRank()
+			_hand.Cards = _hand.Cards[:5]
 			return true, _hand
 		}
 	}
@@ -26,7 +96,6 @@ func (c *Classer) IsStraight(_hand *Hand) (bool, *Hand) {
 	for i := 1; i < l; i++ {
 		prev := cards[i-1]
 		card := cards[i]
-
 		if prev.Rank-1 == card.Rank || (prev.Rank == deck.ACE && card.Rank == deck.KING) {
 			inARow += 1
 		} else if prev.Rank == card.Rank {
@@ -42,4 +111,9 @@ func (c *Classer) IsStraight(_hand *Hand) (bool, *Hand) {
 		}
 	}
 	return false, _hand
+}
+
+func (hc HandClass) String() string {
+	s := []string{"Royal Flush", "Straight Flush", "Four of a Kind", "Full House", "Flush", "Straight", "Three of a Kind", "Two Pairs", "Pair", "No Pair"}
+	return fmt.Sprintf("%s", s[hc])
 }
--- tests/internal/hand/class_test.go
+++ tests/internal/hand/class_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/jqwez/pokergopher/internal/deck"
 	"github.com/jqwez/pokergopher/internal/hand"
-	"github.com/jqwez/pokergopher/tests/internal/util"
 )
 
 func TestNewClasser(t *testing.T) {
@@ -13,24 +12,64 @@ func TestNewClasser(t *testing.T) {
 }
 
 func TestIsFlush(t *testing.T) {
-	h := hand.NewHand()
-	card1, err := deck.NewCard(deck.SPADE, deck.TEN)
-	util.CheckFatal(err)
-	card2, err := deck.NewCard(deck.SPADE, deck.TEN)
-	util.CheckFatal(err)
-	card3, err := deck.NewCard(deck.SPADE, deck.TEN)
-	util.CheckFatal(err)
-	card4, err := deck.NewCard(deck.SPADE, deck.TEN)
-	util.CheckFatal(err)
-	card5, err := deck.NewCard(deck.SPADE, deck.TEN)
-	util.CheckFatal(err)
-	cards := []*deck.Card{card1, card2, card3, card4, card5}
-	for _, card := range cards {
-		h.AddCard(card)
+	hands := []deck.Cards{
+		{
+			{Suit: deck.SPADE, Rank: deck.ACE},
+			{Suit: deck.SPADE, Rank: deck.FIVE},
+			{Suit: deck.SPADE, Rank: deck.SEVEN},
+			{Suit: deck.SPADE, Rank: deck.KING},
+			{Suit: deck.CLUB, Rank: deck.TEN},
+		},
+		{
+			{Suit: deck.SPADE, Rank: deck.TEN},
+			{Suit: deck.SPADE, Rank: deck.JACK},
+			{Suit: deck.SPADE, Rank: deck.QUEEN},
+			{Suit: deck.SPADE, Rank: deck.KING},
+			{Suit: deck.HEART, Rank: deck.ACE},
+		},
+		{
+			{Suit: deck.SPADE, Rank: deck.ACE},
+			{Suit: deck.SPADE, Rank: deck.TWO},
+			{Suit: deck.SPADE, Rank: deck.THREE},
+			{Suit: deck.SPADE, Rank: deck.FOUR},
+			{Suit: deck.CLUB, Rank: deck.FIVE},
+		},
+		{
+			{Suit: deck.SPADE, Rank: deck.ACE},
+			{Suit: deck.HEART, Rank: deck.TWO},
+			{Suit: deck.CLUB, Rank: deck.THREE},
+			{Suit: deck.SPADE, Rank: deck.TWO},
+			{Suit: deck.SPADE, Rank: deck.THREE},
+			{Suit: deck.SPADE, Rank: deck.FOUR},
+			{Suit: deck.CLUB, Rank: deck.FIVE},
+			{Suit: deck.SPADE, Rank: deck.KING},
+		},
+		{
+			{Suit: deck.SPADE, Rank: deck.TEN},
+			{Suit: deck.SPADE, Rank: deck.JACK},
+			{Suit: deck.SPADE, Rank: deck.QUEEN},
+			{Suit: deck.SPADE, Rank: deck.KING},
+			{Suit: deck.SPADE, Rank: deck.THREE},
+			{Suit: deck.HEART, Rank: deck.NINE},
+		},
+		{
+			{Suit: deck.SPADE, Rank: deck.EIGHT},
+			{Suit: deck.SPADE, Rank: deck.JACK},
+			{Suit: deck.SPADE, Rank: deck.QUEEN},
+			{Suit: deck.SPADE, Rank: deck.KING},
+			{Suit: deck.SPADE, Rank: deck.THREE},
+			{Suit: deck.HEART, Rank: deck.NINE},
+		},
 	}
+	results := []bool{false, false, false, true, true, true}
 	classer := hand.NewClasser()
-	if res, _ := classer.IsFlush(h); !res {
-		t.Fatal("Hand should be flush!", h.Cards)
+	for i, cards := range hands {
+		_h := hand.NewHand()
+		_h.Cards = cards
+		res, h := classer.IsFlush(_h)
+		if res != results[i] {
+			t.Fatal("Hand should be flush!", h.Cards)
+		}
 	}
 }
 
@@ -91,6 +130,87 @@ func TestIsStraight(t *testing.T) {
 		res, _ := classer.IsStraight(_hand)
 		if res != results[i] {
 			t.Fatal("Hand should not be", res, "for straight", h)
+		}
+	}
+}
+
+func TestFindBestHand(t *testing.T) {
+
+	hands := []struct {
+		Cards deck.Cards
+		Class hand.HandClass
+	}{
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.ACE},
+				{Suit: deck.SPADE, Rank: deck.FIVE},
+				{Suit: deck.SPADE, Rank: deck.SEVEN},
+				{Suit: deck.SPADE, Rank: deck.KING},
+				{Suit: deck.CLUB, Rank: deck.TEN},
+			},
+			Class: hand.NOPAIR,
+		},
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.TEN},
+				{Suit: deck.SPADE, Rank: deck.JACK},
+				{Suit: deck.SPADE, Rank: deck.QUEEN},
+				{Suit: deck.SPADE, Rank: deck.KING},
+				{Suit: deck.SPADE, Rank: deck.ACE},
+			},
+			Class: hand.ROYAL,
+		},
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.ACE},
+				{Suit: deck.SPADE, Rank: deck.TWO},
+				{Suit: deck.SPADE, Rank: deck.THREE},
+				{Suit: deck.SPADE, Rank: deck.FOUR},
+				{Suit: deck.CLUB, Rank: deck.FIVE},
+			},
+			Class: hand.STRAIGHT,
+		},
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.ACE},
+				{Suit: deck.HEART, Rank: deck.TWO},
+				{Suit: deck.CLUB, Rank: deck.THREE},
+				{Suit: deck.SPADE, Rank: deck.TWO},
+				{Suit: deck.CLUB, Rank: deck.THREE},
+				{Suit: deck.HEART, Rank: deck.FOUR},
+				{Suit: deck.CLUB, Rank: deck.FIVE},
+			},
+			Class: hand.STRAIGHT,
+		},
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.TEN},
+				{Suit: deck.SPADE, Rank: deck.JACK},
+				{Suit: deck.SPADE, Rank: deck.QUEEN},
+				{Suit: deck.SPADE, Rank: deck.KING},
+				{Suit: deck.SPADE, Rank: deck.THREE},
+				{Suit: deck.HEART, Rank: deck.NINE},
+			},
+			Class: hand.FLUSH,
+		},
+		{
+			Cards: deck.Cards{
+				{Suit: deck.SPADE, Rank: deck.EIGHT},
+				{Suit: deck.SPADE, Rank: deck.JACK},
+				{Suit: deck.SPADE, Rank: deck.QUEEN},
+				{Suit: deck.SPADE, Rank: deck.KING},
+				{Suit: deck.SPADE, Rank: deck.THREE},
+				{Suit: deck.HEART, Rank: deck.NINE},
+			},
+			Class: hand.FLUSH,
+		},
+	}
+	for i, testCase := range hands {
+		h := hand.NewHand()
+		h.Cards = testCase.Cards
+		classer := hand.NewClasser().SetHand(h)
+		if testCase.Class != classer.Class {
+			t.Fatal(i, " : Evaluated to ", classer.Class, " but should be ", testCase.Class)
 		}
 	}
 }
